500564f03b0deeb654375a06877e9c7d1016ba92
SonataGirl <37026944+Sonata-Girl@users.noreply.github.com>
2022-08-17 00:08:46 +0300

    Initial Commit

---
+++ TMS_DZ6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMS_DZ6
+//
+//  Created by Sonata Girl on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
